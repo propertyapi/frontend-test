e33ff15aaa57d1aa0653132eb27435a6ca4989e7
Ali Elsheikh <47045107+AliEl4eikh@users.noreply.github.com>
2022-03-02 00:02:54 +0200

    Change the color of the button component when hover over it

--- src/components/button.ts
+++ src/components/button.ts
@@ -13,6 +13,12 @@ background: #1890ff;
 text-shadow: 0 -1px 0 rgb(0 0 0 / 12%);
 box-shadow: 0 2px #0000000b;
 cursor: pointer;
+
+&:hover {
+  background-color: #40a9ff;
+  border-color: #40a9ff;
+}
+
 &:hover ~ ${Container} {
     display: block;
   }
