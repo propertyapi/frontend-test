9f59ac3cb3eb96586ee3e1c70b66ccf2037ae4ea
Ali Elsheikh <47045107+AliEl4eikh@users.noreply.github.com>
2022-03-02 02:37:05 +0200

    Update Button component

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,27 +1,17 @@
 import styled from "styled-components";
-import Container from "./container";
 
 const Button = styled.button`
-height: 32px;
-margin-top: 8px;
-margin-left: 8px;
-padding: 4px 15px;
-color: #fff;
-border: 1px solid #1890ff;
-border-radius: 2px;
-background: #1890ff;
-text-shadow: 0 -1px 0 rgb(0 0 0 / 12%);
-box-shadow: 0 2px #0000000b;
-cursor: pointer;
-
-&:hover {
-  background-color: #40a9ff;
-  border-color: #40a9ff;
-}
-
-&:hover ~ ${Container} {
-    display: block;
-  }
+  height: 32px;
+  margin-top: 8px;
+  margin-left: 8px;
+  padding: 4px 15px;
+  color: #fff;
+  border: 1px solid #1890ff;
+  border-radius: 2px;
+  background: #1890ff;
+  text-shadow: 0 -1px 0 rgb(0 0 0 / 12%);
+  box-shadow: 0 2px #0000000b;
+  cursor: pointer;  
 `
 
 export default Button;
