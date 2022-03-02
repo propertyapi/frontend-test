4d3114614e8e73287edfe1971bf71b423d7abe44
Ali Elsheikh <47045107+AliEl4eikh@users.noreply.github.com>
2022-03-02 22:10:54 +0200

    Update the way of selecting element with React hooks useRef and useLayOutEffect
    Instead of selecting elements with plain JavaScript selectors I used React hooks to be able to select multiple elements

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -1,89 +1,99 @@
-import React, { useState,ReactNode, useEffect } from "react";
-import ContentContainer from "./contentContainer";
-import Content from "./innerContent";
-import Title from "./title";
+import React, { useState, ReactNode, useRef, useLayoutEffect } from "react"
+import ContentContainer from "./contentContainer"
+import Content from "./innerContent"
+import Title from "./title"
 import Button from "./button"
 
 interface PopoverProps {
-    trigger: string;
-    title: ReactNode | (() => ReactNode);
-    content: ReactNode | (() => ReactNode);
-    placement: string;
-};
-
-function Popover(props: PopoverProps) {
-
-    const [isOpen, setIsOpen] = useState(false);
-    const [bottom, setBottom] = useState(0);
-    const [top, setTop] = useState(0);
-    const [left, setLeft] = useState(0);
-    const [right, setRight] = useState(0);
-    const [width, setWidth] = useState(0);
-    const [height, setHeight] = useState(0);
-
-    const {trigger, title, content, placement,...otherProps} = props;
-
-    if (!title && !content) {
-        return null;
-    }
-
-   function handleClick () {
-        setIsOpen(prevState => !prevState);
-        const elem = document.getElementsByClassName("popover-button")[0];
-        const {width, right, left, top, bottom, height} = elem.getBoundingClientRect();
-        setTop(prevState => top)
-        setBottom(prevState => bottom)
-        setRight(prevState => right)
-        setLeft(prevState => left)
-        setHeight(prevState => height)
-        setWidth(prevState => width)
-   }
-
-   function handleMouseEnter() {
-       setIsOpen(true);
-       const elem = document.getElementsByClassName("popover-button")[0];
-        const { right, bottom, width, height} = elem.getBoundingClientRect();
-        setTop(prevState => top)
-        setBottom(prevState => bottom)
-        setRight(prevState => right)
-        setLeft(prevState => left)
-        setHeight(prevState => height)
-        setWidth(prevState => width)
-   }
-    return (
-       <div className="container">
-            {
-                trigger === "click" ? 
-                <Button 
-                className="popover-button"
-                onClick={handleClick}>
-                    Click Me
-                </Button> :
-                <Button
-                className="popover-button"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={() => setIsOpen(false)}
-                >
-                    Hover Me
-                </Button>
-            }
-            { isOpen && 
-                <ContentContainer 
-                    className={placement}
-                    right={right}
-                    left={left}
-                    top={top}
-                    bottom={bottom}
-                    width={width}
-                    height={height}
-                >
-                    <Title>{title}</Title>
-                    <Content>{content}</Content>
-                </ContentContainer>
-              
-            }
-       </div>
-    )
+  trigger: string
+  title: ReactNode | (() => ReactNode)
+  content: ReactNode | (() => ReactNode)
+  placement: string
 }
 
-export default Popover;
+export interface Coords {
+  top: number
+  right: number
+  bottom: number
+  left: number
+  width: number
+  height: number
+  x: number
+  y: number
+}
+
+function Popover(props: PopoverProps) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const [coords, setCoords] = useState<Coords>({
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0
+  })
+
+  const { trigger, title, content, placement, ...otherProps } = props
+
+  const getBoundingClientRect = (element: Element) => {
+    const { top, right, bottom, left, width, height, x, y } = element.getBoundingClientRect()
+    return { top, right, bottom, left, width, height, x, y }
+  }
+
+  const clickedElem = useRef<HTMLButtonElement>(null)
+  const hoveredElem = useRef<HTMLButtonElement>(null)
+
+  useLayoutEffect(() => {
+    function handleClick() {
+      setIsOpen((prevState) => !prevState)
+
+      if (clickedElem.current) {
+        const coords = getBoundingClientRect(clickedElem.current)
+        setCoords(coords)
+      }
+    }
+
+    function handleMouseOver() {
+      setIsOpen(true)
+      if (hoveredElem.current) {
+        const coords = getBoundingClientRect(hoveredElem.current)
+        setCoords(coords)
+      }
+    }
+
+    function handleMouseLeave() {
+      setIsOpen(false)
+    }
+
+    clickedElem.current?.addEventListener("click", handleClick)
+    hoveredElem.current?.addEventListener("mouseenter", handleMouseOver)
+    hoveredElem.current?.addEventListener("mouseleave", handleMouseLeave)
+
+    return () => {
+      clickedElem.current?.removeEventListener("click", handleClick)
+      hoveredElem.current?.removeEventListener("mouseenter", handleMouseOver)
+      hoveredElem.current?.removeEventListener("mouseleave", handleMouseLeave)
+    }
+  })
+
+  if (!title && !content) {
+    return null
+  }
+
+  return (
+    <div className="container">
+      {trigger === "click" ? <Button ref={clickedElem}>Click Me ({placement})</Button> : <Button ref={hoveredElem}>Hover Me</Button>}
+      {isOpen && (
+        <ContentContainer className={placement} coords={coords}>
+          <Title>{title}</Title>
+          <Content>{content}</Content>
+        </ContentContainer>
+      )}
+    </div>
+  )
+}
+
+export default Popover
