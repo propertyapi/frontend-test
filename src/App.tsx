d292e3f62a640f896e40e0c6026808c97e785284
Ali Elsheikh <47045107+AliEl4eikh@users.noreply.github.com>
2022-03-03 02:07:03 +0200

    style: Make the global style in a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,6 @@
 import React from "react"
-import { createGlobalStyle } from "styled-components"
 import Popover from "./components/Popover"
-
-const GlobalStyle = createGlobalStyle`
-body {
-  margin: 0;
-  height: 100vh;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  box-sizing: border-box;
-}
-
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
-
- main, .container, #root, #root>div {
-  height: 100%;
-}
-
-main {
-  display: flex;
-  justify-content: space-around;
-}
-
-.popover {
-  display: flex;
-  align-items: center;
-}
-`
+import GlobalStyle from "./components/GlobalStyle"
 
 function App() {
   const content = (
@@ -49,7 +17,7 @@ function App() {
       <GlobalStyle />
       <main>
         <Popover title={title} content={content} placement="top" trigger="click" />
-        <Popover title={title} content={content} placement="topLeft" trigger="click" />
+        <Popover title={title} content={content} placement="topLeft" trigger="hover" />
         <Popover title={title} content={content} placement="topRight" trigger="click" />
         <Popover title={title} content={content} placement="bottom" trigger="click" />
       </main>
---
+++ src/components/GlobalStyle.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  height: 100vh;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+ main, .container, #root, #root>div {
+  height: 100%;
+}
+
+main {
+  display: flex;
+  justify-content: space-around;
+}
+
+.popover {
+  display: flex;
+  align-items: center;
+}
+`
+export default GlobalStyle
